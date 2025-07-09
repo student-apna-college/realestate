7fac8cb404f7f44c404fb0d8740b6bafd50ead29
Your Name <devender140196@example.com>
2025-07-09 12:48:51 +0530

    changes on app.jsx for .JSX

--- shubhawas/Frontend/vite-project/src/App.jsx
+++ shubhawas/Frontend/vite-project/src/App.jsx
@@ -1,30 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './auth/Login';
-import Ragister from './auth/Ragister';
-import PublicHome from './pages/PublicHome';
-import AdminDashboard from './pages/AdminDashboard';
-import SuperAdminDashboard from './pages/SuperAdminDashboard';
-import ProductCreate from './pages/ProductCreate';
-import Navbar from './pages/Navbar'
+import Login from './auth/Login.jsx';
+import Ragister from './auth/Ragister.jsx';
+import PublicHome from './pages/PublicHome.jsx';
+import AdminDashboard from './pages/AdminDashboard.jsx';
+import SuperAdminDashboard from './pages/SuperAdminDashboard.jsx';
+import ProductCreate from './pages/ProductCreate.jsx';
+import Navbar from './pages/Navbar.jsx'
 // import ShopProducts from './pages/ShopProducts';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import CartPage from './pages/CartPage'
-import About from './pages/About';
-import Contact from './pages/Contact';
-import CreatShop from './pages/CreatShop'
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import CreatShop from './pages/CreatShop.jsx'
 
 
-import CheckoutCOD from './pages/CheckoutCOD';
-import OrderSuccess from './pages/OrderSuccess';
-import MyOrders from './pages/MyOrders';
-import AdminOrders from './pages/AdminOrders';
-import Terms from './pages/Terms&Conditions/Terms';
-import VendorOnboarding from './pages/Terms&Conditions/VendorOnboarding.JSX';
-import ShopRagisterForm from './pages/ShopRagisterForm';
-import SuperAdminShopManager from './pages/SuperAdminShopManager';
-import ContactQuery from './pages/ContectQuery';
-import ShopRagisterQuery from './pages/ShopRagisterQuery';
-import CompanyProperties from './pages/CompanyProperties';
+import CheckoutCOD from './pages/CheckoutCOD.jsx';
+import OrderSuccess from './pages/OrderSuccess.jsx';
+import MyOrders from './pages/MyOrders.jsx';
+import AdminOrders from './pages/AdminOrders.jsx';
+import Terms from './pages/Terms&Conditions/Terms.jsx';
+import VendorOnboarding from './pages/Terms&Conditions/VendorOnboarding.jsx';
+import ShopRagisterForm from './pages/ShopRagisterForm.jsx';
+import SuperAdminShopManager from './pages/SuperAdminShopManager.jsx';
+import ContactQuery from './pages/ContectQuery.jsx';
+import ShopRagisterQuery from './pages/ShopRagisterQuery.jsx';
+import CompanyProperties from './pages/CompanyProperties.jsx';
 
 
 
